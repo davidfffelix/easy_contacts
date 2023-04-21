1376bb248dc61aeebab9d6c2d7aabc8b76ec214b
David Felix <davidffelix@hotmail.com>
2023-04-21 10:47:39 -0300

    Insert FutureBuilder

--- lib/contact_list_home_page.dart
+++ lib/contact_list_home_page.dart
@@ -7,24 +7,6 @@ import 'my_app.dart';
 class ContactListHomePage extends StatelessWidget {
   const ContactListHomePage({super.key});
 
-  static const list = [
-    {
-      'name': 'José',
-      'telephone': '(11) 9 9123-0212',
-      'avatar': 'https://cdn.pixabay.com/photo/2014/04/02/14/11/male-306408_960_720.png',
-    },
-    {
-      'name': 'Maria',
-      'telephone': '(11) 9 9632-7719',
-      'avatar': 'https://cdn.pixabay.com/photo/2014/04/02/14/10/female-306407_960_720.png',
-    },
-    {
-      'name': 'Isabel',
-      'telephone': '(21) 9 7777-8218',
-      'avatar': 'https://cdn.pixabay.com/photo/2014/04/02/14/10/female-306407_960_720.png',
-    }
-  ];
-
   Future<List<Map<String, dynamic>>> _search() async {
     String pathDataBase = join(await getDatabasesPath(), 'database');
     Database db = await openDatabase(
@@ -37,57 +19,68 @@ class ContactListHomePage extends StatelessWidget {
         db.execute(insert3);
       },
     );
-    return db.query('contact'); 
+    return db.query('contact');
   }
+
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        appBar: AppBar(
-          title: const Text('Lista de Contatos'),
-          actions: [
-            Padding(
-              padding: const EdgeInsets.only(right: 20),
-              child: IconButton(
-                icon: const Icon(Icons.add),
-                onPressed: () {
-                  Navigator.of(context).pushNamed(MyApp.contactForm);
-                  // Navigator.pushNamed(context, 'contact-form');
+    return FutureBuilder(
+      future: _search(),
+      builder: (context, future) {
+        if (future.hasData) {
+          var list = future.data;
+          return SafeArea(
+            child: Scaffold(
+              appBar: AppBar(
+                title: const Text('Lista de Contatos'),
+                actions: [
+                  Padding(
+                    padding: const EdgeInsets.only(right: 20),
+                    child: IconButton(
+                      icon: const Icon(Icons.add),
+                      onPressed: () {
+                        Navigator.of(context).pushNamed(MyApp.contactForm);
+                        // Navigator.pushNamed(context, 'contact-form');
+                      },
+                    ),
+                  ),
+                ],
+              ),
+              body: ListView.builder(
+                itemCount: list!.length,
+                itemBuilder: (context, index) {
+                  var contact = list[index];
+                  Widget avatar = CircleAvatar(
+                    backgroundImage: NetworkImage(contact['url_avatar']!),
+                  );
+                  return ListTile(
+                    leading: avatar,
+                    title: Text(contact['name']),
+                    subtitle: Text(contact['telephone']),
+                    trailing: Container(
+                      width: 100,
+                      child: Row(
+                        children: [
+                          IconButton(
+                            icon: const Icon(Icons.edit),
+                            onPressed: () {},
+                          ),
+                          IconButton(
+                            icon: const Icon(Icons.delete),
+                            onPressed: () {},
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
                 },
               ),
             ),
-          ],
-        ),
-        body: ListView.builder(
-          itemCount: list.length,
-          itemBuilder: (context, index) {
-            var contact = list[index];
-            Widget avatar = CircleAvatar(
-              backgroundImage: NetworkImage(contact['avatar']!),
-            );
-            return ListTile(
-              leading: avatar,
-              title: Text(contact['name']!),
-              subtitle: Text(contact['telephone']!),
-              trailing: Container(
-                width: 100,
-                child: Row(
-                  children: [
-                    IconButton(
-                      icon: const Icon(Icons.edit),
-                      onPressed: () {},
-                    ),
-                    IconButton(
-                      icon: const Icon(Icons.delete),
-                      onPressed: () {},
-                    ),
-                  ],
-                ),
-              ),
-            );
-          },
-        ),
-      ),
+          );
+        } else {
+          return const Scaffold();
+        }
+      },
     );
   }
 }
