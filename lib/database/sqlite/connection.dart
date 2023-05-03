8b47895e1893f1679ee098fc0a3fe45b75030dfd
David Felix <davidffelix@hotmail.com>
2023-05-02 21:23:01 -0300

    Review project

--- lib/database/sqlite/connection.dart
+++ lib/database/sqlite/connection.dart
@@ -1,13 +1,13 @@
-import 'package:path/path.dart';
-import 'package:sqflite/sqflite.dart';
+// import 'package:path/path.dart';
+// import 'package:sqflite/sqflite.dart';
 
-class Connection {
-  static Database _db;
+// class Connection {
+//   static Database _db;
 
-  static Future<Database> get() async {
-    if (_db == null) {
-      var path = join(getDatabasesPath(), 'contacts_bank');
-    }
-    return _db;
-  }
-}
+//   static Future<Database> get() async {
+//     if (_db == null) {
+//       var path = join(getDatabasesPath(), 'contacts_bank');
+//     }
+//     return _db;
+//   }
+// }
